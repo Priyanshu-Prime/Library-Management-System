97039b9064e9ad1c44236e5b4c9588501941938a
Priyanshu-Prime <pmedu718@gmail.com>
2024-09-27 20:38:13 +0530

    add .env and setup db.js

---
+++ backend/config/db.js
@@ -0,0 +1,28 @@
+const {Pool} = require('pg');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const pool = new Pool
+(
+    {
+        user : process.env.PG_USER,
+        host : process.env.PG_HOST,
+        database : process.env.PG_DATABASE,
+        password : process.env.PG_PASSWORD,
+        port : process.env.PG_PORT
+    }
+);
+
+pool.connect((err) =>
+{
+    if(err)
+    {
+        console.log("Error connecting to the database pool", err.message);
+        // console.log(err);
+    }
+
+    else
+        console.log("Connected to Postgres DB successfully");
+});
+
+module.exports = pool;
