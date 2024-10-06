a25d12ae2d9ce6ab4348ea51ad73d63104c93e0b
Shashi-Preetam1 <shashipreetam00@gmail.com>
2024-10-06 23:49:41 +0530

    fixed account_info page overflow

--- client/src/pages/accountInfo/AccountInfo.jsx
+++ client/src/pages/accountInfo/AccountInfo.jsx
@@ -30,12 +30,12 @@ const AccountInfo = () => {
     return (
         <div className="h-screen w-screen flex">
             <Sidebar />
-            <div className="h-full w-4/5 bg-[#A1EEC5] flex flex-col">
+            <div className="h-full w-4/5 bg-[#A1EEC5] flex flex-col overflow-y-auto">
             <InventoryTopBar />
             {students.length > 0 ? (
-                    <div className="w-full">
+                    <div className="h-5/6 w-full">
                         {students.map((student) => (
-                            <div key={student.id} className="p-4 mb-2 rounded shadow">
+                            <div key={student.id} className="p-4 rounded shadow">
                                 <h3 className="text-lg font-bold">{student.name}</h3>
                                 <p className="text-gray-700">Student ID: {student.roll_no}</p>
                                 <p className="text-gray-700">Student Email: {student.email}</p>
