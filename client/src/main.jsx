bac90d8947a46b82320a2ea4ae2654eb4d68f706
Priyanshu-Prime <pmedu718@gmail.com>
2024-10-06 23:44:54 +0530

    add google login button frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GoogleOAuthProvider clientId="161736225122-pl6len1i4d9ovvclob36b9vm6d33cqcb.apps.googleusercontent.com">
     <App />
+    </GoogleOAuthProvider>;
   </StrictMode>,
 )
