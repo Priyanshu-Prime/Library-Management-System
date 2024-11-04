0bc95e4d2c66169b261fac7f748f49d4b504bd1e
Priyanshu-Prime <pmedu718@gmail.com>
2024-11-04 23:23:51 +0530

    try account fix (failed)

--- client/src/pages/accountInfo/AccountInfo.jsx
+++ client/src/pages/accountInfo/AccountInfo.jsx
@@ -8,14 +8,23 @@ import axios from 'axios';
 const PORT = import.meta.env.VITE_SERVER_PORT;
 
 const AccountInfo = () => {
+    
+    const [uid, setUID] = useState('');
+  useEffect(() => {
+    const id = localStorage.getItem('uid');
+    if (id)
+      setUID(id);
+  }, []);
     const [students, setStudents] = useState([]) 
-
+    console.log(uid);
     const fetchBooks = async () => {
-        const url = `http://localhost:${PORT}/api/students`
+        const url = `http://localhost:${PORT}/api/students/${uid}`
 
         try {
             const response = await axios.get(url)
+            console.log(`DATA:  ${response.data}`);
             setStudents(response.data)
+            console.log(`YE LE BSDK: ${(students)}`);
             console.log(students)
         }
         catch(error) {
@@ -34,13 +43,19 @@ const AccountInfo = () => {
             <InventoryTopBar />
             {students.length > 0 ? (
                     <div className="h-5/6 w-full">
-                        {students.map((student) => (
+                        {/* {students.map((student) => (
                             <div key={student.id} className="p-4 rounded shadow">
                                 <h3 className="text-lg font-bold">{student.name}</h3>
                                 <p className="text-gray-700">Student ID: {student.roll_no}</p>
                                 <p className="text-gray-700">Student Email: {student.email}</p>
                             </div>
-                        ))}
+                        ))} */}
+                        <div key={students.id} className="p-4 rounded shadow">
+                            <h3 className="text-lg font-bold">{students.name}</h3>
+                            <p className="text-gray-700">Student ID: {students.roll_no}</p>
+                            <p className="text-gray-700">Student Email: {students.email}</p>
+                        </div>
+                        
                     </div>
                 ) : (
                     <p>No students available.</p>
