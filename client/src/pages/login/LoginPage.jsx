8479405fdc60b23560c5aa628ab58263f03715fc
Vedant_1 <vedantande.45@gmail.com>
2025-05-08 18:28:14 +0530

    Login Fixed

--- client/src/pages/login/LoginPage.jsx
+++ client/src/pages/login/LoginPage.jsx
@@ -1,20 +1,17 @@
+import { Input } from "postcss";
 import React from "react";
 import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+import LoginButton from "../../components/LoginButton";
+
+const PORT = import.meta.env.VITE_SERVER_PORT;
 
 const LoginPage = () => {
   return (
     <div className="h-screen w-screen flex bg-[#2274A5] justify-center items-center">
       <div className="h-2/5 w-1/3 bg-white rounded-2xl flex flex-col justify-center items-center shadow-lg">
         <div className="text-4xl font-bold text-[#2274A5] mb-8">LOGIN</div>
-        <GoogleLogin
-          theme="filled_blue"
-          onSuccess={(credentialResponse) => {
-            console.log("Login Success:", credentialResponse);
-          }}
-          onError={() => {
-            console.log("Login Failed");
-          }}
-        />
+        <LoginButton />
       </div>
     </div>
   );
